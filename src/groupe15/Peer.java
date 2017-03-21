25b41e0810542cfa5d0b0a94f80386c3b0a01b0b
Bousnadji Soria <soria.bousnadji@etu.u-pec.fr>
2017-03-21 17:39:26 +0100

    peer commit2

--- src/groupe15/Peer.java
+++ src/groupe15/Peer.java
@@ -1,6 +1,7 @@
 package groupe15;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -12,6 +13,7 @@ import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.Charset;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class Peer {
@@ -38,7 +40,8 @@ public class Peer {
 			System.out.println("Erreur démarrage serveur");
 		}
 	}
-	public static void sendPeerList(SocketChannel socketChannel) {
+	public void sendPeerList(SocketChannel socketChannel) {
+		System.out.print("Envoi liste des pairs...");
 		ByteBuffer toWrite = ByteBuffer.allocate(70000);
 		Charset c = Charset.forName("UTF-8");
 		toWrite.clear();
@@ -57,139 +60,142 @@ public class Peer {
 		}
 		toWrite.flip();
 		try {
-			System.out.println(toWrite);
-			socketChannel.write(toWrite);
-			toWrite.clear();
+			if(socketChannel.write(toWrite) > 0){
+				toWrite.clear();
+				System.out.println("   [OK]");
+				System.out.println();
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	public static void sendFilesList(SocketChannel socketChannel) {
+	public void sendFilesList(SocketChannel socketChannel) {
+		System.out.print("Envoi liste des fichiers...");
 		ByteBuffer toWrite = ByteBuffer.allocate(70000);
 		Charset c = Charset.forName("UTF-8");
 		toWrite.clear();
 		Map<String, Long> filesList = new HashMap<>();
-//		filesList.put("serveurTa3Ch3er.txt", (long)487);
-//		filesList.put("JSK.txt", (long)535);
-		filesList.put("hohohoho.txt", (long)299);
+//		filesList.put("filefiletest.txt", (long)1029);
+		filesList.put("imaz.png", (long)106089);
+//		filesList.put("lefth.txt", (long)364);
 //		filesList.put("Z_IMAZIGHEN.jpg", (long)4675);
-//		filesList.put("MATOUB.jpg.tar.gz", (long)4301);
+//		filesList.put("Projet2017.pdf", (long)135987);
 		toWrite.put((byte)5);
 		int nbFiles = filesList.size();
 		toWrite.putInt(nbFiles);
 		for (Map.Entry<String, Long> entry : filesList.entrySet()) {
 			long fileSize = (long)entry.getValue();
 			String nameFileT = entry.getKey();
-			System.out.println(nameFileT);
 			ByteBuffer nameFile = c.encode(nameFileT);
-			System.out.println("name file   :   "+nameFile);
 			int stringSize = nameFile.limit();
 //			nameFile.flip();
 			
-			System.out.println(stringSize);
 			toWrite.putInt(stringSize);
 			toWrite.put(nameFile);
 			toWrite.putLong((long)fileSize);
 		}
 		toWrite.flip();
-		System.out.println("writerrr : "+toWrite);
 		try {
-			socketChannel.write(toWrite);
-			toWrite.clear();
+			if(socketChannel.write(toWrite) > 0){
+				toWrite.clear();
+				System.out.println("   [OK]");
+				System.out.println();
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
 	public static ByteBuffer sliceBuffer(ByteBuffer byteBuffer, long offset, int sizeFragment){
-		System.out.println("origin  "+byteBuffer);
-
-//		byteBuffer.flip();
 		ByteBuffer bufferReturn = ByteBuffer.allocate(sizeFragment);
 		bufferReturn.clear();
 		byteBuffer.position((int)offset);
 		int lastPosition = (int)offset + sizeFragment;
 		while (byteBuffer.position() < lastPosition) {
-			System.out.println("origin  "+byteBuffer);
-			System.out.println("temp  "+bufferReturn);
 			bufferReturn.put(byteBuffer.get());
 		}
 		return bufferReturn;
 	}
-	
+	public void sendFile(SocketChannel channel, ByteBuffer toRead) throws IOException{
+		Charset c = Charset.forName("UTF-8");
+		int stringSize = toRead.getInt();		//taile du nom du fichier
+		ByteBuffer tempFileName = ByteBuffer.allocate(1024);
+		int p = toRead.position()+stringSize;
+		while (toRead.position() != p) {		//get nom fichier
+			tempFileName.put(toRead.get());
+		}
+		tempFileName.flip();
+		CharBuffer cb = c.decode(tempFileName);
+		String fileNameChar = cb.toString();
+		System.out.print("Envoi du fichier "+fileNameChar+" en cours...");
+		long totalSize = toRead.getLong();
+		long posRead = toRead.getLong();
+		int fragmentSize = toRead.getInt();
+		ByteBuffer toWrite = ByteBuffer.allocate(70000);
+		toWrite.clear();
+		toWrite.put((byte)7);
+		toWrite.putInt(stringSize);
+		ByteBuffer fileNameByte = c.encode(fileNameChar);
+		toWrite.put(fileNameByte);
+		toWrite.putLong(totalSize);
+		toWrite.putLong(posRead);
+		toWrite.putInt(fragmentSize);
+		
+		FileInputStream fis = new FileInputStream("/home/faredj/workspace/"+fileNameChar);
+		byte[] fileInput = new byte[fis.available()];
+		fis.read(fileInput);
+		fis.close();
+		ByteBuffer bufForAllFile = ByteBuffer.wrap(fileInput);
+		ByteBuffer newByteBuffer = ByteBuffer.allocate(fragmentSize);
+		newByteBuffer = sliceBuffer(bufForAllFile, posRead, fragmentSize);
+		newByteBuffer.flip();
+		toWrite.put(newByteBuffer);
+		toWrite.flip();
+		if(channel.write(toWrite) > 0){
+			toWrite.clear();
+			System.out.println("   [OK]");
+			System.out.println();
+		}
+		
+	}
 	public void run() throws IOException {
 		while (true) {
 			selector.select();
-			//selector.selectNow();
-			for (SelectionKey key : selector.selectedKeys()) {
+			Iterator<SelectionKey> keyIt = selector.selectedKeys().iterator();
+			while (keyIt.hasNext()) {
+				SelectionKey key = keyIt.next();
 				if(key.isAcceptable()){
 					SocketChannel client = server.accept();
-					System.out.println(client);
-					client.configureBlocking(false);
-					client.register(selector, SelectionKey.OP_READ);
+					if(client != null){
+						client.configureBlocking(false);
+						client.register(selector, SelectionKey.OP_READ);
+						System.out.println("Nouveau client : "+client.getRemoteAddress());
+					}
 				}
-				
 				if(key.isReadable()){
-					Charset c = Charset.forName("UTF-8");
 					SocketChannel channel = (SocketChannel) key.channel();
 					ByteBuffer toRead = ByteBuffer.allocate(1024);
 					toRead.clear();
 					channel.read(toRead);
 					toRead.flip();
 					int id = (int)toRead.get();
-					System.out.println(">>>>>>>>    "+id);
 					switch (id) {
 						case 2:
 							System.out.println("Id "+id+" : Demande de liste des paires connus");
-							sendPeerList(channel);
+							this.sendPeerList(channel);
 							break;
 						case 4:
 							System.out.println("Id "+id+" : Demande de liste des fichiers connus");
-							sendFilesList(channel);
+							this.sendFilesList(channel);
 							break;
 						case 6:
 							System.out.println("Id "+id+" : Demande fichier");
-							System.out.println();
-							int stringSize = toRead.getInt();		//taile du nom du fichier
-							ByteBuffer tempFileName = ByteBuffer.allocate(1024);
-							int p = toRead.position()+stringSize;
-							while (toRead.position() != p) {		//get nom fichier
-								tempFileName.put(toRead.get());
-							}
-							tempFileName.flip();
-							CharBuffer cb = c.decode(tempFileName);
-							String fileNameChar = cb.toString();
-							System.out.println("nom size : "+stringSize);
-							System.out.println("file "+fileNameChar);
-							long totalSize = toRead.getLong();
-							long posRead = toRead.getLong();
-							int fragmentSize = toRead.getInt();
-							ByteBuffer toWrite = ByteBuffer.allocate(70000);
-							toWrite.clear();
-							toWrite.put((byte)7);
-							toWrite.putInt(stringSize);
-							ByteBuffer fileNameByte = c.encode(fileNameChar);
-							toWrite.put(fileNameByte);
-							toWrite.putLong(totalSize);
-							toWrite.putLong(posRead);
-							toWrite.putInt(fragmentSize);
-							
-							FileInputStream fis = new FileInputStream("/home/faredj/workspace/"+fileNameChar);
-							byte[] fileInput = new byte[fis.available()];
-							fis.read(fileInput);
-							ByteBuffer bufForAllFile = ByteBuffer.wrap(fileInput);
-							ByteBuffer newByteBuffer = ByteBuffer.allocate(fragmentSize);
-							newByteBuffer = sliceBuffer(bufForAllFile, posRead, fragmentSize);
-							System.out.println("final   "+newByteBuffer);
-							newByteBuffer.flip();
-							toWrite.put(newByteBuffer);
-							toWrite.flip();
-							System.out.println(toWrite);
-							channel.write(toWrite);
+							this.sendFile(channel, toRead);
 							break;
 						default:break;
 					}
 				}
+				keyIt.remove();
 			}
 		}
 	}
@@ -197,6 +203,5 @@ public class Peer {
 		System.out.println("Démarrage serveur");
 		Peer peer = new Peer(4433);
 		peer.run();
-		System.out.println("Serveur démarré");
 	}
 }
